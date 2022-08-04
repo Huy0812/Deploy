f897c680ba13765cce5649d53d4c9e6f301a49e3
unknown <trantuanhuy181611@gmail.com>
2022-08-04 10:31:02 +0700

    update

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const app = require('./app');
 const dotenv = require("dotenv")
 const connectDatabase = require("./config/database")
 const cloudinary = require("cloudinary")
-
+const CLOUD_NAME = "dkg6au9i8"
+const CLOUD_API_KEY = "661768686494964"
+const CLOUD_API_SECRET = "GTscWyzjFQd3mulJpUxYKk__KnM"
 dotenv.config({
     path: "./config/config.env"
 })
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+    cloud_name: CLOUD_NAME,
+    api_key: CLOUD_API_KEY,
+    api_secret: CLOUD_API_SECRET
 })
 connectDatabase()
 app.listen(process.env.PORT, () => {
