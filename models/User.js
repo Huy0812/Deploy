3a3e9d45041483932331da46cc074e4a5e1c80e6
unknown <trantuanhuy181611@gmail.com>
2022-08-11 09:35:14 +0700

    update

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     // SĐT
     phoneNumber: {
         type: String,
-        match: /^(0)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/,
+        //match: /^(0)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/,
         required: true,
         unique: true,
     },
