bf587dee9b96714b205c0e1cafbf9e095ff00dfc
unknown <trantuanhuy181611@gmail.com>
2022-08-16 16:30:21 +0700

    update

--- routers/Company.js
+++ routers/Company.js
@@ -1,8 +1,9 @@
 const express = require("express")
-const { create, getInformation, updateInformation } = require("../controllers/Company")
+const { create, getInformation, updateInformation, updateCompanyIp } = require("../controllers/Company")
 const isAuthenticated = require("../middleware/auth")
 const router = express.Router()
 router.route("/create").post(create)
 router.route("/information").get(isAuthenticated, getInformation)
 router.route("/updateinformation").put(updateInformation)
+router.route("/updatecompanyip").put(isAuthenticated, updateCompanyIp)
 module.exports = router
